17cff7de4803fcd73e18ededd43913eb5d032ae0
Максим Горбатюк <maxim993511@gmail.com>
2022-05-11 13:58:08 +0300

    13 57

--- Program.cs
+++ Program.cs
@@ -13,54 +13,12 @@ namespace Labas_2
            
             if (a == 2)
             {
-                int[][] arr = GenerateJuggedArray(4, 4);
-                int[][] newArr = new int[arr.Length][];
-                PrintJuggedArray(arr);
-                int min = FindMin(arr);
-                int count = 0;
-                Console.WriteLine(min);
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    if (!NeedToDelete(arr[i], min))
-                    {
-                        newArr[count] = arr[i];
-                        count++;
-                    }
-                }
-                Array.Resize(ref newArr, count);
-                Console.WriteLine("////////////////");
-                PrintJuggedArray(newArr);
+               
             }
         
 
         }
-        static bool NeedToDelete(int[] arr, int min)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] == min)
-                    return true;
-                }
-                return false;
-        }
-
-       
         
-        static int FindMin(int[][] arr)
-        {
-            int min = int.MaxValue;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = 0; j < arr[i].Length; j++)
-                {
-                    if (arr[i][j] < min)
-                    {
-                        min = arr[i][j];
-                    }
-                }
-            }
-            return min;
-        }
 
 
 
